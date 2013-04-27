76c9fa9ada017fa673727fb578e8a3d30304ef21
Beatriz Gonzalez <beatgonza@gmail>
2013-04-26 23:11:26 -0600

    Reader registration happy path - strong parameter not working

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -3,4 +3,16 @@ class ReadersController < ApplicationController
   def new
     @reader = Reader.new
   end
+
+  def create
+    reader = Reader.new(params[:reader])
+    reader.save
+    redirect_to root_url, notice: "You registered"
+  end
+
+  private
+
+  def reader_params
+    params.require(:reader).permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,5 @@
 class Reader < ActiveRecord::Base
+
+  attr_accessible :email, :password, :password_confirmation
   attr_accessor :password, :password_confirmation
 end
